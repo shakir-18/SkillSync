84de665b887effaa2af6ed08b73397a16c0df762
Shakir <shakir4236@gmail.com>
2026-06-06 21:21:46 +0530

    Initial Commit

--- src/main/java/com/hackathon/SkillSync/controller/LearningPlannerController.java
+++ src/main/java/com/hackathon/SkillSync/controller/LearningPlannerController.java
@@ -1,4 +1,4 @@
 package com.hackathon.SkillSync.controller;
 
-public class EmployeeController {
+public class LearningPlannerController {
 }
---
+++ src/test/java/com/hackathon/SkillSync/SkillSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.SkillSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
